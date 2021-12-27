99e343fca427e1b1568669a063007c7f2610863d
jkingsman <jack.kingsman@gmail.com>
2021-12-26 21:42:47 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {
+    Server
+} = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/movie', (req, res) => {
+    res.sendFile(__dirname + '/movie.mp4');
+});
+
+io.on('connection', (socket) => {
+    socket.on('action', msg => {
+        socket.broadcast.emit('action', msg);
+    });
+
+    socket.on('heartbeat', msg => {
+        socket.broadcast.emit('heartbeat', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
